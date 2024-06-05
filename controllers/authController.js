3ba7a7d3416c28e174a2a3f0682648056af72b8f
Giftmasil <masilagift@gmail.com>
2024-06-05 10:59:43 +0300

    second commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,7 +31,7 @@ const handleLogin = async (req, res) => {
         foundUser.refreshToken = refreshToken;
         const result = await foundUser.save();
         //saving the refresh token in a cookie
-        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: false, maxAge: 24 * 60 * 60 * 1000 }); // change it later to true
+        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 }); // change it later to true
         res.json({ accessToken });
     } else {
         res.sendStatus(401);
